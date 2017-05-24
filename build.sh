bade233d12a9dae76944b2f9c4e6242ab6fc2c3a
suve <veg@svgames.pl>
2017-05-24 18:35:47 +0200

    Add copyright note to build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
+#
+# build.sh - install script for vrms-rpm
+# Copyright (C) 2017 Artur "suve" Iwicki
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License, version 3,
+# as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program (LICENCE.txt). If not, see <http://www.gnu.org/licenses/>.
+#
 
+# Treat unitiliased variables as errors
 set -u
 
+# Set default values for vars
 mode="build"
 prefix=""
 
