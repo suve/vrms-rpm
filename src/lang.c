43dd34b62a02500e272199e26bdd2484cfe611af
suve <veg@svgames.pl>
2018-05-20 11:15:57 +0200

    Add initial translation support

---
+++ src/lang.c
@@ -0,0 +1,66 @@
+/**
+ * vrms-rpm - list non-free packages on an rpm-based Linux distribution
+ * Copyright (C) 2018 Artur "suve" Iwicki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (LICENCE.txt). If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <libintl.h>
+#include <locale.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "lang.h"
+
+#define GENERATE_STRING(what) #what,
+static const char *const msgname[] = {
+	FOREACH_MESSAGE(GENERATE_STRING)
+};
+
+
+void lang_init(void) {
+	textdomain("vrms-rpm");
+	
+	// Take a look at the test string and change locale to English
+	// if there isn't a translation available
+	const char *teststr = lang_getmsg(MSG_TEST_STRING);
+	if(strcmp(teststr, msgname[MSG_TEST_STRING]) == 0) setlocale(LC_MESSAGES, "en");
+}
+
+char* lang_getmsg(const enum MessageID msgid) {
+	return gettext(msgname[msgid]);
+}
+
+int lang_printmsg(const enum MessageID msgid, ...) {
+	char *msgstr = lang_getmsg(msgid);
+	if(msgstr == NULL) return -1;
+	
+	va_list args;
+	va_start(args, msgstr);
+	int bytes = vprintf(msgstr, args);
+	va_end(args);
+	
+	return bytes;
+}
+
+int lang_snprintmsg(char *buffer, const size_t bufsize, const enum MessageID msgid, ...) {
+	char *msgstr = lang_getmsg(msgid);
+	if(msgstr == NULL) return -1;
+	
+	va_list args;
+	va_start(args, msgstr);
+	int bytes = vsnprintf(buffer, bufsize, msgstr, args);
+	va_end(args);
+	
+	return bytes;
+}
---
+++ src/lang.h
@@ -0,0 +1,40 @@
+/**
+ * vrms-rpm - list non-free packages on an rpm-based Linux distribution
+ * Copyright (C) 2018 Artur "suve" Iwicki
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program (LICENCE.txt). If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef VRMS_RPM_LANG_H
+#define VRMS_RPM_LANG_H
+
+#include <string.h>
+
+#define FOREACH_MESSAGE(MESSAGE)  \
+   MESSAGE(TEST_STRING) \
+   
+
+#define GENERATE_ENUM(what) MSG_ ## what, 
+enum MessageID {
+   FOREACH_MESSAGE(GENERATE_ENUM)
+};
+#undef GENERATE_ENUM
+
+
+void lang_init(void);
+
+char* lang_getmsg(const enum MessageID msgid);
+
+int lang_printmsg(const enum MessageID msgid, ...);
+int lang_snprintmsg(char *buffer, const size_t bufsize, const enum MessageID msgid, ...);
+
+#endif
