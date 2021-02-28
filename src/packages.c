8f60da806bcb9364010bed074b9ae40937ea5135
suve <veg@svgames.pl>
2021-02-28 17:48:13 +0100

    Detect duplicate package names and compare EVR
    It is possible for a package to have multiple versions installed
    at the same time. The two most common examples are:
    a) "kernel" and associated packages - many distros keep at least 2
       of those, so you can easily boot up an older kernel, if the newest
       one causes some issues
    b) multilib packages (i.e. having both an x86_64 and i686 build
       of the same library)
    
    Until now, vrms-rpm ignored this, displaying all packages by name only.
    This commit makes vrms-rpm ask for epoch-version-release.arch information
    for each package, and when duplicate packages are detected, print this
    info to eliminate ambiguity as to which package it is describing.

--- src/packages.c
+++ src/packages.c
@@ -1,6 +1,6 @@
 /**
  * vrms-rpm - list non-free packages on an rpm-based Linux distribution
- * Copyright (C) 2018 Artur "suve" Iwicki
+ * Copyright (C) 2018, 2021 Artur "suve" Iwicki
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License, version 3,
@@ -30,10 +30,10 @@
 struct Pipe* packages_openPipe(void) {
 	char *queryformat;
 	if(!opt_describe)
-		queryformat = "%{NAME}\\t%{LICENSE}\\n";
+		queryformat = "%{NAME}\\t%{EPOCH}\\t%{VERSION}\\t%{RELEASE}\\t%{ARCH}\\t%{LICENSE}\\n";
 	else
-		queryformat = "%{NAME}\\t%{LICENSE}\\t%{SUMMARY}\\n";
-	
+		queryformat = "%{NAME}\\t%{EPOCH}\\t%{VERSION}\\t%{RELEASE}\\t%{ARCH}\\t%{LICENSE}\\t%{SUMMARY}\\n";
+
 	char *args[] = {
 		"/usr/bin/rpm",
 		"--all",
@@ -42,14 +42,15 @@ struct Pipe* packages_openPipe(void) {
 		queryformat,
 		(char*)NULL
 	};
-	
+
 	return pipe_create(args);
 }
 
+
 struct Package {
-	char *name;
+	char *name, *summary;
+	char *epoch, *release, *version, *arch;
 	struct LicenceTreeNode *licence;
-	char *summary;
 };
 
 #define LIST_COUNT      (list->used / sizeof(struct Package))
@@ -75,56 +76,72 @@ static int init_buffers(void) {
 	return 0;
 }
 
-static int split_line(char *line, char **name, char **licence, char **summary) {
-	char *tab;
+static int split_line(char *line, char ***fields, int max_fields) {
+	*(fields[0]) = line;
+	for(int i = 1; i < max_fields; ++i) *(fields[i]) = NULL;
+
+	char *search_pos = line;
+
+	int count = 1;
+	while(count < max_fields) {
+		char* tab = strchr(search_pos, '\t');
+		if(tab == NULL) break;
+
+		*tab = '\0';
+		*(fields[count]) = search_pos = (tab + 1);
+		count += 1;
+	}
 	
-	#define setptr(ptr, val) { if((ptr) != NULL) { (*ptr) = (val); }}
-	setptr(name, line);
-	setptr(licence, NULL);
-	setptr(summary, NULL);
-	
-	tab = strchr(line, '\t');
-	if(!tab) return 1;
-	
-	*tab = '\0';
-	setptr(licence, tab+1);
-	
-	tab = strchr(tab+1, '\t');
-	if(!tab) return 2;
-	
-	*tab = '\0';
-	setptr(summary, tab+1);
-	
-	return 3;
+	return count;
 }
 
-int packages_read(struct Pipe *pipe) {
+extern int packages_read(struct Pipe *pipe) {
 	if(init_buffers() != 0) return -1;
 	sorted = 0;
-	
+
 	FILE *f = pipe_fopen(pipe);
 	if(f == NULL) return -1;
-	
-	char line[256];
+
 	char *name, *licence, *summary;
-	
+	char *epoch, *version, *release, *arch;
+
+	const int expected = opt_describe ? 7 : 6;
+	char **fields[] = {
+		&name, &epoch, &version, &release, &arch, &licence, &summary
+	};
+
+	char line[512];
 	while(fgets(line, sizeof(line), f) != NULL) {
 		replace_unicode_spaces(line);
-		
-		const int segments_expected = opt_describe ? 3 : 2;
-		if(split_line(line, &name, &licence, &summary) != segments_expected) continue;
-		
+		if(split_line(line, fields, expected) != expected) continue;
+
 		name = chainbuf_append(&buffer, name);
 		licence = chainbuf_append(&buffer, trim(licence, NULL));
 		if(opt_describe) summary = chainbuf_append(&buffer, trim(summary, NULL));
-		
+
+		// Epoch is typically undefined. RPM reports this using the special string "(none)".
+		// Avoid storing unnecessary epoch info by comparing epoch with this special string.
+		if(strcmp(epoch, "(none)") != 0) {
+			epoch = chainbuf_append(&buffer, epoch);
+		} else {
+			epoch = NULL;
+		}
+
+		version = chainbuf_append(&buffer, version);
+		release = chainbuf_append(&buffer, release);
+		arch = chainbuf_append(&buffer, arch);
+
 		struct LicenceTreeNode *classification = licence_classify(licence);
 		class_count[classification->is_free] += 1;
 		
 		struct Package pkg = {
 			.name = name,
+			.summary = summary,
+			.epoch = epoch,
+			.version = version,
+			.release = release,
+			.arch = arch,
 			.licence = classification,
-			.summary = summary
 		};
 		if(rebuf_append(list, &pkg, sizeof(struct Package)) == NULL) return -1;
 	}
@@ -156,7 +173,37 @@ static int pkgcompare(const void *A, const void *B) {
 	const struct Package *a = A;
 	const struct Package *b = B;
 	
-	return strcasecmp(a->name, b->name);
+	int compare_names = strcasecmp(a->name, b->name);
+	if(compare_names) return compare_names;
+
+	const char* pairs[] = {
+		a->epoch, b->epoch,
+		a->version, b->version,
+		a->release, b->release,
+		a->arch, b->arch,
+	};
+	for(unsigned int p = 0; p < sizeof(pairs) / sizeof(pairs[0]) / 2; ++p) {
+		const char *pair_a = pairs[p*2];
+		const char *pair_b = pairs[p*2 + 1];
+
+		// If the value is undefined for both packages, skip comparing it.
+		// If it's defined only for one package, assume that any_value > no_value.
+		if(pair_a == NULL) {
+			if(pair_b == NULL) {
+				continue;
+			}
+			return -1;
+		} else {
+			if(pair_b == NULL) {
+				return +1;
+			}
+		}
+
+		// Temporary logic before we switch to using librpm
+		int compare_pair = strcasecmp(pair_a, pair_b);
+		if(compare_pair) return compare_pair;
+	}
+	return 0;
 }
 
 static void packages_sort(void) {
@@ -189,16 +236,46 @@ static void printnode(struct LicenceTreeNode *node) {
 	}
 }
 
+static void format_evr(char *evrbuf, const size_t bufsize, const struct Package *pkg) {
+	if(pkg->epoch != NULL)
+		snprintf(evrbuf, bufsize, "-%s:%s-%s.%s", pkg->epoch, pkg->version, pkg->release, pkg->arch);
+	else
+		snprintf(evrbuf, bufsize, "-%s-%s.%s", pkg->version, pkg->release, pkg->arch);
+}
+
 static void printlist(const int which_kind) {
+	int duplicate_next = 0;
+
 	const int count = LIST_COUNT;
 	for(int i = 0; i < count; ++i) {
 		struct Package *pkg = &LIST_ITEM(i);
 		if(pkg->licence->is_free != which_kind) continue;
-		
-		if(opt_describe) {
-			printf(" - %s: %s", pkg->name, pkg->summary);
+
+		/*
+		 * Compare with next package to determine whether this is a duplicate.
+		 * Since we sorted the packages by name earlier, duplicates are guaranteed
+		 * to be located next to each other in the list.
+		 *
+		 * If this is not a duplicate of the next package, check whether the previous
+		 * package set the "next package is a duplicate" flag.
+		 */
+		int duplicate_this;
+		if((i != (count-1)) && (strcasecmp(pkg->name, LIST_ITEM(i+1).name) == 0)) {
+			duplicate_next = duplicate_this = 1;
 		} else {
-			printf(" - %s", pkg->name);
+			duplicate_this = duplicate_next;
+			duplicate_next = 0;
+		}
+
+		// If this is a duplicate, print also the epoch-version-release numbers
+		// to reduce ambiguity as to which package we're describing.
+		char evr[128] = {'\0'};
+		if(duplicate_this) format_evr(evr, sizeof(evr), pkg);
+
+		if(opt_describe) {
+			printf(" - %s%s: %s", pkg->name, evr, pkg->summary);
+		} else {
+			printf(" - %s%s", pkg->name, evr);
 		}
 		
 		if(opt_explain) {
