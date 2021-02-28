1f22761264b941ad73357f755fd48ad3ce4f8aa5
suve <veg@svgames.pl>
2021-02-28 19:54:36 +0100

    Compare versions by segments in fallback mechanism
    Previously, the fallback version comparison mechanism would just
    compare version strings as, well, strings. The new mechanism
    works as follows:
    1) Split each string into segments on dots ('.').
    2) Attempt to convert the pair of segments into integers.
       a) If this succeeds, compare their numerical value.
       b) Otherwise, compare them as strings.
    3) If this pair of segments is deemed equal, compare the next one.
    4) If one string has more segments than the other,
       the longer string is condidered greater.
    
    While still a lot simpler than the mechanism employed by RPM,
    this algorithm should lead to a more natural ordering of packages.

--- src/versions.c
+++ src/versions.c
@@ -18,13 +18,82 @@
 #include "versions.h"
 
 #ifdef WITH_LIBRPM
-	#include <rpm/rpmio.h>
-	#include <rpm/rpmlib.h>
+#include <rpm/rpmio.h>
+#include <rpm/rpmlib.h>
+
 #else
-	#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void next_segment(const char **verstr, char *buffer, const size_t bufsize) {
+	int segment_len;
+	int jump_len;
+
+	char *dot = strchr(*verstr, '.');
+	if(dot != NULL) {
+		segment_len = (dot - *verstr);
+		jump_len = segment_len + 1;
+	} else {
+		jump_len = segment_len = strlen(*verstr);
+	}
+
+	snprintf(buffer, bufsize, "%.*s", segment_len, *verstr);
+	*verstr += jump_len;
+}
+
+static int str_to_int(const char *str, long *result) {
+	char *badchar;
+	*result = strtol(str, &badchar, 10);
+	return (badchar != NULL) && (*badchar == '\0');
+}
+
+static int fallback_compare(const char *a, const char *b) {
+	char buffer_a[64];
+	char buffer_b[64];
+
+	for(;;) {
+		// If we exhausted one of the version strings,
+		// but the other one still has some segments,
+		// consider the longer one to be greater.
+		// If we exhausted both, consider them equal.
+		if(*a == '\0') {
+			if(*b == '\0') {
+				return 0;
+			}
+			return -1;
+		} else {
+			if(*b == '\0') {
+				return +1;
+			}
+		}
+
+		// Retrieve the next segment of each version string.
+		next_segment(&a, buffer_a, sizeof(buffer_a));
+		next_segment(&b, buffer_b, sizeof(buffer_b));
+
+		// Attempt to convert the segments to integers.
+		// If this succeeds, compare their numerical values.
+		long int a_int, b_int;
+		const int a_is_int = str_to_int(buffer_a, &a_int);
+		const int b_is_int = str_to_int(buffer_b, &b_int);
+		if(a_is_int && b_is_int) {
+			if(a_int > b_int) return +1;
+			if(a_int < b_int) return -1;
+			continue;
+		}
+
+		// If segments are not ints, compare them as strings.
+		const int compare_strings = strcmp(buffer_a, buffer_b);
+		if(compare_strings != 0) return compare_strings;
+	}
+}
 #endif
 
 int compare_versions(const char *a, const char *b) {
+	// Avoid passing NULLs to comparison functions.
+	// If one value is NULL, but the other is not,
+	// consider the non-NULL one to be greater.
 	if(a == NULL) {
 		if(b == NULL) {
 			return 0;
@@ -39,7 +108,6 @@ int compare_versions(const char *a, const char *b) {
 	#ifdef WITH_LIBRPM
 		return rpmvercmp(a, b);
 	#else
-		// TODO: Add a better fallback mechanism
-		return strcmp(a, b);
+		return fallback_compare(a, b);
 	#endif
 }
