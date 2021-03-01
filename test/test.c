c509ae3953c99a9dd241ae8d6164662f6498ab27
suve <veg@svgames.pl>
2021-03-01 22:05:25 +0100

    Add tests for str_starts_with() and str_ends_with()

--- test/test.c
+++ test/test.c
@@ -25,31 +25,76 @@
 
 #define UNUSED(x) ((void)(x))
 
-static void test_trim_case(const char *input, const char *expected, const char *extrachars) {
-	size_t input_len = strlen(input);
-	char buffer[input_len+1];
-	memcpy(buffer, input, input_len+1);
+#define test__trim__case(input, expected, extrachars) do { \
+	char buffer[] = input; \
+\
+	size_t result_len; \
+	char *result = trim_extra(buffer, &result_len, extrachars); \
+\
+	assert_string_equal(result, expected); \
+	assert_int_equal((int)result_len, (int)strlen(result)); \
+}while(0)
 
-	size_t result_len;
-	char *result = trim_extra(buffer, &result_len, extrachars);
-
-	assert_string_equal(result, expected);
-	assert_int_equal((int)result_len, (int)strlen(result));
-}
-
-static void test_trim(void **state) {
+static void test__trim(void **state) {
 	UNUSED(state);
 
-	test_trim_case("newline\n", "newline", "");
-	test_trim_case("    indent", "indent", "");
-	test_trim_case("\tfield-one\tfield-two\t", "field-one\tfield-two", "");
-	test_trim_case("(some extra chars)", "some extra chars", "()");
-	test_trim_case("remove everything", "", "abcdefghijklmnopqrstuvwxyz");
+	test__trim__case("newline\n", "newline", "");
+	test__trim__case("    indent", "indent", "");
+	test__trim__case("\tfield-one\tfield-two\t", "field-one\tfield-two", "");
+	test__trim__case("(some extra chars)", "some extra chars", "()");
+	test__trim__case("remove everything", "", "abcdefghijklmnopqrstuvwxyz");
+}
+
+#define test__str_starts_with__case(haystack, needle, expected) do{ \
+	const char *result = str_starts_with(haystack, needle); \
+	if(expected) { \
+		assert_non_null(result); \
+		assert_ptr_equal(haystack, result); \
+	} else { \
+		assert_null(result); \
+	} \
+}while(0)
+
+static void test__str_starts_with(void **state) {
+	UNUSED(state);
+
+	test__str_starts_with__case("partial match", "partial", 1);
+	test__str_starts_with__case("full string match", "full string match", 1);
+	test__str_starts_with__case("an empty needle", "", 1);
+
+	test__str_starts_with__case("no match", "blah", 0);
+	test__str_starts_with__case("very", "very long needle", 0);
+	test__str_starts_with__case("", "an empty haystack", 0);
+}
+
+#define test__str_ends_with__case(haystack, needle, expected) do{ \
+	const char *result = str_ends_with(haystack, needle); \
+	if(expected) { \
+		assert_non_null(result); \
+		assert_string_equal(result, needle); \
+	} else { \
+		assert_null(result); \
+	} \
+}while(0)
+
+static void test__str_ends_with(void **state) {
+	UNUSED(state);
+
+	test__str_ends_with__case("partial match", "match", 1);
+	test__str_ends_with__case("full string match", "full string match", 1);
+	test__str_ends_with__case("an empty needle", "", 1);
+
+	test__str_ends_with__case("no match", "blah", 0);
+	test__str_ends_with__case("very", "very long needle", 0);
+	test__str_ends_with__case("longer", "match at end but needle is longer", 0);
+	test__str_ends_with__case("", "an empty haystack", 0);
 }
 
 int main(void) {
 	const struct CMUnitTest tests[] = {
-		cmocka_unit_test(test_trim),
+		cmocka_unit_test(test__trim),
+		cmocka_unit_test(test__str_starts_with),
+		cmocka_unit_test(test__str_ends_with),
 	};
 	return cmocka_run_group_tests(tests, NULL, NULL);
 }
