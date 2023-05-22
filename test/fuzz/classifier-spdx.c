9c490a109cdb26545757d477236b5402f384aa69
Marcin Radomski <marcin@mradomski.pl>
2023-05-22 22:48:46 +0200

    Fuzz test: use ReBuffer

--- test/fuzz/classifier-spdx.c
+++ test/fuzz/classifier-spdx.c
@@ -20,51 +20,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const size_t CHUNK_SIZE = 4096;
-
-struct buffer {
-	char* data;
-	size_t capacity;
-	size_t size;
-};
-
-void buffer_cleanup(struct buffer* buf) {
-	free(buf->data);
-	memset(buf, 0, sizeof(*buf));
-}
-
-int buffer_append(struct buffer* buf, const char* data, size_t size) {
-	if (buf->size + size > buf->capacity) {
-		size_t new_capacity = buf->size + size;
-		if (new_capacity % CHUNK_SIZE) {
-			new_capacity = ((new_capacity / CHUNK_SIZE) + 1) * CHUNK_SIZE;
-		}
-		char* new_data = realloc(buf->data, new_capacity);
-		if (!new_data) {
-			return -1;
-		}
-		buf->data = new_data;
-		buf->capacity = new_capacity;
-	}
-
-	memcpy(&buf->data[buf->size], data, size);
-	buf->size += size;
-	return 0;
-}
+#include "buffers.h"
 
 char* read_stdin() {
-	struct buffer buf = {0};
+	const size_t CHUNK_SIZE = 4096;
+	struct ReBuffer* buf = rebuf_init();
 
 	while (!feof(stdin)) {
 		char chunk[CHUNK_SIZE];
 		size_t read = fread(chunk, 1, sizeof(chunk), stdin);
-		if (buffer_append(&buf, chunk, read)) {
-			buffer_cleanup(&buf);
+		if (rebuf_append(buf, chunk, read)) {
+			rebuf_free(buf);
 			return NULL;
 		}
 	}
-	buffer_append(&buf, "", 1);
-	return buf.data;
+	rebuf_append(buf, "", 1);
+
+	char* result = buf.data;
+	buf.data = NULL;
+	rebuf_free(buf);
+	return result;
 }
 
 int main(int argc, char* argv[]) {
