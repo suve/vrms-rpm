8d70cd7e1357c9c74f51eb2eea00cc8855c88b9c
suve <veg@svgames.pl>
2023-08-01 21:59:14 +0200

    Add Fedora support in update-licence-lists script

--- utils/update-licence-lists.php
+++ utils/update-licence-lists.php
@@ -33,17 +33,88 @@ function read_json_file($url) {
 	return $json;
 }
 
-function array_key_is_true($array, $key) {
-	if(!array_key_exists($key, $array)) return false;
-	return (bool)($array[$key]);
-}
-
 function write_to_file($filename, $data) {
 	$data = implode("\n", $data) . "\n"; // Make sure file ends with a newline
 	if(file_put_contents($filename, $data) === false) {
 		fprintf(STDERR, "Failed to write to file \"%s\"\n", $filename);
 		die(1);
 	}
+}
+
+function get_nested_value($jsonObj, ...$keys) {
+	foreach($keys as $key) {
+		if(!array_key_exists($key, $jsonObj)) return NULL;
+		$jsonObj = $jsonObj[$key];
+	}
+
+	return $jsonObj;
+}
+
+function allowed_in_fedora($licence) {
+	$statusList = get_nested_value($licence, 'license', 'status');
+	if($statusList !== NULL) {
+		// The proper way
+		foreach($statusList as $status) {
+			switch($status) {
+				case 'allowed':
+				case 'allowed-content':
+				case 'allowed-documentation':
+				case 'allowed-fonts':
+					return true;
+
+				case 'not-allowed':
+					return false;
+
+				default:
+					fprintf(STDERR, "Unknown Fedora licence status: \"%s\"\n", $status);
+					die(1);
+			}
+		}
+	} else {
+		// The deprecated way
+		$approved = get_nested_value($licence, 'approved');
+		if($approved === "yes") return true;
+		if($approved === "no") return false;
+	}
+
+	fprintf(STDERR, "Failed to determine Fedora licence status:\n%s\n", print_r($licence, true));
+	die(1);
+}
+
+function update_fedora() {
+	$fedoraSourceUrl = 'https://gitlab.com/fedora/legal/fedora-license-data/-/jobs/artifacts/main/raw/fedora-licenses.json?job=json';
+	$fedoraData = read_json_file($fedoraSourceUrl);
+
+	$list = [];
+	foreach($fedoraData as $licence) {
+		if(!allowed_in_fedora($licence)) continue;
+
+		// The proper way
+		$spdxExpr = get_nested_value($licence, 'license', 'expression');
+		if($spdxExpr !== NULL) $list[] = $spdxExpr;
+
+		$legacyAbbrevs = get_nested_value($licence, 'fedora', 'legacy-abbreviation');
+		if($legacyAbbrevs !== NULL) {
+			foreach($legacyAbbrevs as $abbrev) $list[] = $abbrev;
+		}
+
+		// The deprecated way
+		$spdxExpr = get_nested_value($licence, 'spdx_abbrev');
+		if($spdxExpr !== NULL) $list[] = $spdxExpr;
+
+		$legacyAbbr = get_nested_value($licence, 'fedora_abbrev');
+		if($legacyAbbr !== NULL) $list[] = $legacyAbbr;
+	}
+
+	// TODO: Probably need to write custom function for case-insensitive sort+uniq
+	$list = array_unique($list, SORT_STRING);
+	sort($list, SORT_STRING | SORT_FLAG_CASE);
+	write_to_file('../licences/fedora.txt', $list);
+}
+
+function array_key_is_true($array, $key) {
+	if(!array_key_exists($key, $array)) return false;
+	return (bool)($array[$key]);
 }
 
 function update_spdx() {
@@ -80,5 +151,6 @@ function update_spdx() {
 }
 
 chdir(__DIR__);
+update_fedora();
 update_spdx();
 
