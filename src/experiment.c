ff07df33a0930a04740cb5cd31809dd2f8add38a
suve <veg@svgames.pl>
2026-04-04 22:45:39 +0200

    Initial code for fetching packages from librpm

---
+++ src/experiment.c
@@ -0,0 +1,30 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <rpm/header.h>
+#include <rpm/rpmdb.h>
+#include <rpm/rpmlib.h>
+#include <rpm/rpmts.h>
+
+int main(void) {
+	rpmReadConfigFiles(NULL, NULL);
+
+	rpmts ts = NULL;
+	ts = rpmtsCreate();
+	rpmtsSetFlags(ts, rpmtsFlags(ts) | RPMTRANS_FLAG_NOPLUGINS);
+	rpmdbMatchIterator mi = rpmtsInitIterator(ts, RPMDBI_PACKAGES, NULL, 0);
+	
+	Header header = NULL;
+	while((header = rpmdbNextIterator(mi)) != NULL) {
+		const char *name, *version, *release;
+		name = headerGetString(header, RPMTAG_NAME);
+		version = headerGetString(header, RPMTAG_VERSION);
+		release = headerGetString(header, RPMTAG_RELEASE);
+		printf("%s-%s-%s\n", name, version, release);
+	}
+
+	rpmdbFreeIterator(mi);
+	rpmtsFree(ts);
+	return 0;
+}
